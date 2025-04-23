13f3d078c69e1bad479ab4448b2929611a8da647
sakthiganesh <sakthiganesh451@gmail.com>
2025-04-23 18:02:29 +0530

    I complete razorpay the booking,

--- utils/generateSignature.js
+++ utils/generateSignature.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 const orderId = 'order_QMPfN43yPYj9Oy';
 const paymentId = 'pay_test_12345';
-const keySecret = 'MJuR0muY5MZLHIjTZTpbISNH'; // Replace with your actual key_secret from .env
+const keySecret = ''; // Replace with your actual key_secret from .env
 
 const data = `${orderId}|${paymentId}`;
 const signature = crypto
