fd88815db69e6c8551d133c602daafaeb06369ea
sakthiganesh <sakthiganesh2003@example.com>
2026-03-27 14:07:46 +0530

    Fix Serverless Function crashing on Vercel

--- server.js
+++ server.js
@@ -113,7 +113,9 @@ mongoose
   });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}
 
 module.exports = app;
